4abda21f20945459e67a9465653c34fc3746d098
Bob Silverberg <bob.silverberg@gmail.com>
2015-06-24 10:10:55 -0700

    Fixes #121 - Add a test to open a website from the details page

--- marketplacetests/marketplace/pages/app_details.py
+++ marketplacetests/marketplace/pages/app_details.py
@@ -9,13 +9,14 @@ from marketplacetests.marketplace.pages.base import BasePage
 
 class Details(BasePage):
 
-    _page_loaded_locator = (By.CSS_SELECTOR, 'section.app-reviews')
+    _page_loaded_locator = (By.CSS_SELECTOR, 'section.app-info')
 
     _write_review_locator = (By.CSS_SELECTOR, 'a.review-button')
     _app_info_locator = (By.CSS_SELECTOR, '.detail .info')
     _first_review_locator = (By.CSS_SELECTOR, '.reviews-wrapper li:first-child')
     _first_review_body_locator = (By.CSS_SELECTOR, '.reviews-wrapper .review-body')
     _install_button_locator = (By.CSS_SELECTOR, '.detail .info button.mkt-app-button.install')
+    _open_website_button_locator = (By.CSS_SELECTOR, 'button.mkt-app-button')
 
     @property
     def is_app_details_displayed(self):
@@ -33,6 +34,13 @@ class Details(BasePage):
     @property
     def first_review_rating(self):
         return int(self.marionette.find_element(*self._first_review_locator).get_attribute('data-rating'))
+
+    @property
+    def website_href(self):
+        return self.marionette.find_element(*self._open_website_button_locator).get_attribute('href')
+
+    def tap_open_website_button(self):
+        self.marionette.find_element(*self._open_website_button_locator).tap()
 
     def tap_write_review(self, logged_in=True):
         self.wait_for_element_displayed(*self._write_review_locator)
